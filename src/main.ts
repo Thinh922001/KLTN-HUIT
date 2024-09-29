0e10cedf41685ccee0110af371fea453b5bfb92b
Thinh922001 <lecuongthinh2001@gmail.com>
2024-09-29 18:32:18 +0700

    #12: add helmet

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import compression from 'compression';
 import { initializeTransactionalContext } from 'typeorm-transactional';
+import helmet from 'helmet';
 
 async function bootstrap() {
   initializeTransactionalContext();
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(compression());
+  app.use(helmet());
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
