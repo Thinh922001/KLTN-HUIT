5cd5fb7b9c7a5f7543391dd0b973d6ecc50eef21
Thinh922001 <lecuongthinh2001@gmail.com>
2024-09-29 15:42:45 +0700

    #10: fix cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as compression from 'compression';
 import { initializeTransactionalContext } from 'typeorm-transactional';
 
 async function bootstrap() {
   initializeTransactionalContext();
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use(compression());
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
