d2e98aaaa0ec0c8ec0f7226623017f799c28c46d
Thinh922001 <lecuongthinh2001@gmail.com>
2024-12-05 12:37:25 +0700

    #82: fix cors and api get all coupon

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,18 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   initializeTransactionalContext();
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || origin.includes('localhost') || origin === 'https://kltn-huit-fe.vercel.app') {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization, x-api-key',
+    credentials: true,
+  });
   app.use(compression());
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
