f4d3555c7c4ce368c6f857d4e2d52cc03ea77c64
Thinh922001 <lecuongthinh2001@gmail.com>
2024-12-10 16:19:31 +0700

    #95: fix error send mail

--- src/modules/cron/cron.module.ts
+++ src/modules/cron/cron.module.ts
@@ -4,9 +4,10 @@ import { CronService } from './cron.service';
 import { MailService } from '../mail/mail.service';
 import { StatisticService } from '../statistic/statistic.service';
 import { AdminRepository } from '../../repositories/admin.repository';
+import { InvoiceRepository } from '../../repositories';
 
 @Module({
-  providers: [CronService, MailService, StatisticService, AdminRepository],
+  providers: [CronService, MailService, StatisticService, AdminRepository, InvoiceRepository],
   imports: [ProductModule],
 })
 export class CronModule {}
