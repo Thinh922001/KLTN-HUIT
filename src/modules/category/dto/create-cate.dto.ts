eefc8d2cd2d1536e8a3f3cb85bd1d454ffe3c8ff
Thinh922001 <lecuongthinh2001@gmail.com>
2024-12-12 16:33:15 +0700

    #120: fix add cate

--- src/modules/category/dto/create-cate.dto.ts
+++ src/modules/category/dto/create-cate.dto.ts
@@ -1,11 +1,11 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateCateDto {
   @IsString()
   @IsNotEmpty()
   name: string;
 
-  @IsString()
+  @IsNumber()
   @IsNotEmpty()
   cateTypeId: number;
 }
